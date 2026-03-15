2b7fd404ad130fbb1efd121feddc38be79e52b9e
Erik-J-Mathew21 <georgecilna@gmail.com>
2026-03-15 14:46:19 +1000

    Add files via upload

---
+++ Binary.py
@@ -0,0 +1,7 @@
+number = float(input("Enter a decimal number: "))
+binary = ""
+while number > 0:
+    remainder = number % 2
+    binary = str(remainder) + binary
+    number = number // 2
+print("Binary:", binary)
