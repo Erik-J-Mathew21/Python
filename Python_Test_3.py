ec0bf8b52aa2e78dffc8173cd0d5def4d0962fe7
Erik-J-Mathew21 <georgecilna@gmail.com>
2026-05-31 17:52:12 +1000

    Add files via upload

---
+++ Python_Test_3.py
@@ -0,0 +1,8 @@
+student_dict = {"Charles" : "95", "Alice" : "12", "Tom" : "96", "Darcy" : "87", "Daisy" : "37"}
+num = (100 + 12 + 96 + 87 + 37)
+for i in (student_dict):
+    i = (num/5)
+print(max(student_dict))
+print(min(student_dict))
+student_dict.get("Tom")
+input(student_dict.get("Enter a student name:"))
