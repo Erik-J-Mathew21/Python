8b6ebd01a4e1c091abe724ff0cdf3818a03f4692
Erik-J-Mathew21 <georgecilna@gmail.com>
2026-08-02 17:37:23 +1000

    Add files via upload

---
+++ Book.py
@@ -0,0 +1,24 @@
+class Book:
+    def __init__(self, title, author, is_borrowed):
+        self.title = title
+        self.author = author
+        self.is_borrowed = is_borrowed
+        is_borrowed == False
+        borrow(is_borrowed == True)
+        print("Book is borrowed.")
+        return_book(is_borrowed == False)
+    book1.title("Hello, World!")
+    book1.author("Python")
+    book1.is_borrowed(False)
+    borrow()
+    return_book()
+    book2.title("Scratch Programming")
+    book2.author("Snake")
+    book2.is_borrowed(False)
+    borrow()
+    return_book()
+    book3.title("Python Programming")
+    book3.author("Adder")
+    book3.is_borrowed(False)
+    borrow()
+    return_book()
